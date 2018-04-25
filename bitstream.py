b9a28dd4cb28ef88500c7ae5715163433b67f136
Karol Trzcinski <k.trzcinski95@gmail.com>
2018-04-25 09:57:26 +0200

    dodanie bitstreamera

---
+++ bitstream.py
@@ -0,0 +1,65 @@
+import math
+
+class Bitstream:
+    def __init__(self):
+        self.buf = 0
+        self.len = 0
+
+    def write(self, x, in_data_bits_len):
+        ''' read \in_data_bits_len bits from stream '''
+        assert in_data_bits_len >= 0
+        mask = (1 << in_data_bits_len) - 1
+        self.buf <<= in_data_bits_len
+        self.buf |= (x & mask)
+        self.len += in_data_bits_len
+
+    def writeall(self, in_table, in_data_bits_len):
+        ''' write all data from \in_table to a stream'''
+        for d in in_table:
+            self.write(d, in_data_bits_len)
+
+    def read(self, out_data_bits_len):
+        ''' read \out_data_bits_len bits from stream '''
+        assert out_data_bits_len >= 0
+        if self.len <= 0:
+            return None
+        mask = (1 << out_data_bits_len) - 1
+        offset = self.len - out_data_bits_len
+        readed = self.buf & (mask << offset)
+        ret = readed >> offset
+        self.buf -= readed
+        self.len -= out_data_bits_len
+        return ret
+
+    def readall(self, out_data_bits_len):
+        ''' read all data from stream in \out_data_bits_len chunks '''
+        out_len = math.floor(self.len/out_data_bits_len)
+        return [self.read(out_data_bits_len) for _ in range(out_len)]
+
+if __name__ == "__main__":
+    print("|===============================|")
+    print("|        Bitstream demo         |")
+    print("| Author: Karol Trzciński, 2018 |")
+    print("|===============================|")
+    print("")
+    in_data = [0, 1, 2, 4, 24]
+    in_data_bits_len = 8
+    out_data_bits_len = 4
+
+    # create buffer and write all data
+    stream = Bitstream()
+    stream.write(in_data[0], in_data_bits_len)
+    stream.writeall(in_data[1:], in_data_bits_len)
+
+    # read all data from stream
+    out_first = stream.read(out_data_bits_len)
+    out_data = stream.readall(out_data_bits_len)
+
+    # concantenate readed data
+    out_data = [out_first] + out_data
+
+    # print experiment results
+    print("for a input bits len equal " + str(in_data_bits_len))
+    print("output bits len equal " + str(out_data_bits_len))
+    print("and input data equal " + str(in_data))
+    print("output is " + str(out_data))
